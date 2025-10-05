697f4edb3e8cc8f031aecb3319073749bf97f66f
SaradaPanigrahy <98700255+SaradaPanigrahy@users.noreply.github.com>
2025-10-06 00:36:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const soundButtons = document.querySelectorAll(".btn");
+console.log(soundButtons);
+const stopButton = document.querySelector(".stop");
+
+let currAudio = null;
+
+function playSound(soundFile) {
+
+    if(currAudio) {
+        currAudio.pause();
+        currAudio.currentTime = 0;
+    }
+    
+    currAudio = new Audio(soundFile);
+    currAudio.play();
+    console.log(currAudio);
+}
+
+
+soundButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const soundFile = button.getAttribute("data-sound");
+        playSound(soundFile);
+    })
+})
+
+stopButton.addEventListener("click", () => {
+    console.log(currAudio);
+    if(currAudio) {
+        console.log("audio is paused");
+        currAudio.pause();
+        currAudio.currentTime = 0; //restart the song
+    }
+})
